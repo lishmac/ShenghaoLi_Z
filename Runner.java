0b0b6f8e7ebe97f406f769d49e921bac8cb75299
Shenghao Li <lishmac@Shenghaos-MBP.T-mobile.com>
2017-05-30 12:58:33 -0700

    add N variable
    Signed-off-by: Shenghao Li <lishmac@Shenghaos-MBP.T-mobile.com>

--- Runner.java
+++ Runner.java
@@ -10,9 +10,10 @@ public class Runner {
 		//sample input from your example
 		String[] log = new String[]{"U1", "/", "U1", "subscribers", "U2", "/","U2", "subscribers", "U1", "filter", "U1", "export", "U2", "filter", "U2", "export", "U3", "/", "U3", "catalog", "U3", "edit"};
 		String[] log2 = new String[]{"U1", "/", "U1", "sub", "U1", "export"};
+		int N = 2;
 		Map<String, List<String>> output1 = t1.preProcessing(log);
 		Map<String, Integer> output2 = t1.frequencyMap(output1);
-		String[] result = t1.topNPath(output2, 2);
+		String[] result = t1.topNPath(output2, N);
 		System.out.println(Arrays.toString(result));
 	}
 }
